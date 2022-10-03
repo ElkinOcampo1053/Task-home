3820b9373b2186bed6b7a634da348badb06a0509
Elkin Ocampo <elkinocampo1053@gmail.com>
2022-10-03 17:07:28 -0500

    Eliminar enlaces

--- src/main/java/co/edu/utp/misiontic/g12g4/backend/taskhome/TaskHomeApplication.java
+++ src/main/java/co/edu/utp/misiontic/g12g4/backend/taskhome/TaskHomeApplication.java
@@ -1,13 +1,10 @@
 package co.edu.utp.misiontic.g12g4.backend.taskhome;
 
-import java.util.Arrays;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
 
-import co.edu.utp.misiontic.g12g4.backend.taskhome.model.entity.User;
 import co.edu.utp.misiontic.g12g4.backend.taskhome.model.repository.EmailRepository;
 import lombok.AllArgsConstructor;
 import lombok.Data;
--- src/main/java/co/edu/utp/misiontic/g12g4/backend/taskhome/controller/TaskHomeController.java
+++ src/main/java/co/edu/utp/misiontic/g12g4/backend/taskhome/controller/TaskHomeController.java
@@ -3,23 +3,15 @@ package co.edu.utp.misiontic.g12g4.backend.taskhome.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 
 // Spring MVC
 
 @AllArgsConstructor
 @Controller
-@Slf4j
 public class TaskHomeController {
 
     @GetMapping(value = { "/", "/index", "/index.html" })
